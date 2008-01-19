d6670e67be4db56b02140f12b1451839cbe148f9
Felipe Reyes <felipereyes@gmail.com>
2008-01-19 17:11:47 -0300

    starting to populate the project with the classes of design diagram

---
+++ rascase/core/logical.py
@@ -0,0 +1,28 @@
+##
+## logical.py
+## Login : <freyes@yoda>
+## Started on  Sat Jan 19 17:02:59 2008 Felipe Reyes
+## $Id$
+## 
+## Copyright (C) 2008 Felipe Reyes
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##
+
+import rascase.core.base as base
+
+class LogicalModel(base.ModelBase):
+    def __init__(self):
+        pass
+
