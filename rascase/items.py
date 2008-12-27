f9632052d8cfbf7e5b202887a685e0f2a86697b5
Felipe Reyes <felipereyes@gmail.com>
2008-12-27 13:35:06 -0300

    [items] added items module

---
+++ rascase/items.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+##
+## items.py
+## Login : <freyes@yoda.starwars.cl>
+## Started on  Sat Dec 27 13:33:29 2008 Felipe Reyes
+## $Id$
+##
+## Copyright (C) 2008 Felipe Reyes <felipereyes@gmail.com>
+##
+## This file is part of Rascase.
+##
+## Rascase is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Rascase is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
